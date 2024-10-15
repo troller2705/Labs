8ad16a76a2f19f7affe4e8ae29e04406bddecb31
Troller2705 <cole.groves2705@gmail.com>
2024-10-15 11:22:19 -0400

    Lab 2
    - Added Player Controller
    - Added Sprites
    - Added Level & Level Colliders
    - Moved Camera onto player so it will follow

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [Range(5f, 10f)]
+    public float speed = 5.5f;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2 (hInput * speed, rb.velocity.y);
+        
+    }
+}
